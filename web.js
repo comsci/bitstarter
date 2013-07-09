9c4fedab5e3679707de64db0fa45349c2344237e
Ubuntu <ubuntu@ip-10-212-185-133.ec2.internal>
2013-07-09 14:48:17 +0000

    fix wrong method call

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   // response.send('Hello World 2!');
-  response.send(buffer.toString);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
